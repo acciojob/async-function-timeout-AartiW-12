f320e04c13d1ac554fa224ec37fe31638c9f3aa9
AartiW-12 <aartiwhaval12@gmail.com>
2025-11-29 13:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+
+document.getElementById("btn").addEventListener("click", async ()=>{
+	const textVal = document.getElementById("text").value;
+	const numVal  = document.getElementById("delay").value;
+	const outputDiv = document.getElementById("output")
+	await display(numVal);
+	outputDiv.textContent = textVal
+})
+
+async function display(ms){
+	return new Promise((resolve)=>{
+		setTimeout(resolve,ms)
+	})
+}
